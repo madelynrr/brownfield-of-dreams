340d19973db80be7c7b520d24b258795d85f9dfb
Wren Steitle <lauren.steitle@gmail.com>
2020-02-12 23:27:57 -0700

    Add invite method
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

--- app/mailers/user_activate_mailer.rb
+++ app/mailers/user_activate_mailer.rb
@@ -4,4 +4,10 @@ class UserActivateMailer < ApplicationMailer
     @user_email = user_email
     mail(to: user_email, subject: 'Please activate your account.')
   end
+
+  def invite(user, invitee)
+    @invitee = invitee
+    @user = user
+    mail(to: invitee[:email], subject: 'Welcome to Brownfield.')
+  end
 end
