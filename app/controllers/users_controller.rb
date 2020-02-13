b6dffaf7daa870563fe5519e87ee374576a76deb
Wren Steitle <lauren.steitle@gmail.com>
2020-02-12 20:43:43 -0700

    Add flash notice
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       redirect_to dashboard_path
+      flash[:notice] = "Logged in as #{user.first_name}"
     else
       flash[:error] = 'Username already exists'
       render :new
