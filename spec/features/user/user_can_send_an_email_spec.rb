fe15681d0881c33cb97aa03ec17d62d62b9ff43b
Wren Steitle <lauren.steitle@gmail.com>
2020-02-12 20:40:52 -0700

    Create test for emailing a user
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

---
+++ spec/features/user/user_can_send_an_email_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "as a user", :vcr do
+  it "i get an email to activate" do
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+    password_confirmation = 'password'
+
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up now.'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on'Create Account'
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Logged in as Jim")
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
+  end
+end
