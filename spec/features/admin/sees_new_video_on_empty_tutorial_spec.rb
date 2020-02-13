d51c6c00a3dbfa9e74d105fefb1045ce6d4dd619
Wren Steitle <lauren.steitle@gmail.com>
2020-02-13 01:19:19 -0700

    Add testing for empty tutorials
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

---
+++ spec/features/admin/sees_new_video_on_empty_tutorial_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'An admin user can edit tutorials', :vcr do
+  it 'sees edit on empty tutorial' do
+    admin = create(:user, role: 1)
+    tutorial = create(:tutorial)
+
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/tutorials/#{tutorial.id}"
+    expect(current_path).to eq("/admin/tutorials/#{tutorial.id}/edit")
+  end
+
+  it 'sees tutorial index if empty tutorial' do
+    user = create(:user)
+    tutorial = create(:tutorial)
+
+    tutorial2 = create(:tutorial)
+    video2 = create(:video, tutorial_id: tutorial2.id)
+
+    tutorial3 = create(:tutorial)
+    video3 = create(:video, tutorial_id: tutorial3.id)
+
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/tutorials/#{tutorial.id}"
+    expect(current_path).to eq('/tutorials')
+    expect(page).to have_content('This tutorial is still in progress.')
+  end
+end
