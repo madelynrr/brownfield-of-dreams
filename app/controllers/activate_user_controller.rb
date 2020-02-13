f640195916d8535029777732b20455c0afcdbe07
Wren Steitle <lauren.steitle@gmail.com>
2020-02-12 23:25:14 -0700

    Create invites for new users
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

--- app/controllers/activate_user_controller.rb
+++ app/controllers/activate_user_controller.rb
@@ -1,8 +1,25 @@
 class ActivateUserController < ApplicationController
 
-  def new
+  def update
     email = "#{params[:email]}.#{params[:format]}"
     user = User.find_by(email: email)
     user.toggle!(:active)
+  end
+
+  def new; end
+
+  def create
+    user = current_user
+    service = GithubService.new
+    invitee = service.find_user(user, params[:github_handle])
+    if invitee[:email]
+      @invitee = invitee
+      UserActivateMailer.invite(user, @invitee).deliver_now
+      flash[:notice] = "Successfully sent invite!"
+      redirect_to '/dashboard'
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+      redirect_to '/dashboard'
+    end
   end
 end
