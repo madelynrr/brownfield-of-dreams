88f8b8df120370160eb5ff5dddacb8df42ce8f8a
Wren Steitle <lauren.steitle@gmail.com>
2020-02-12 21:58:40 -0700

    update test for user story
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

--- spec/features/user/user_can_send_an_email_spec.rb
+++ spec/features/user/user_can_send_an_email_spec.rb
@@ -10,13 +10,9 @@ RSpec.describe "as a user", :vcr do
 
     visit '/'
 
-    click_on 'Sign In'
+    click_on 'Register'
 
-    expect(current_path).to eq(login_path)
-
-    click_on 'Sign up now.'
-
-    expect(current_path).to eq(new_user_path)
+    expect(current_path).to eq('/register')
 
     fill_in 'user[email]', with: email
     fill_in 'user[first_name]', with: first_name
