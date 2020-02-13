005e7fe8592c864b4015c78fad8b29df96e18722
Wren Steitle <lauren.steitle@gmail.com>
2020-02-13 01:20:14 -0700

    Add logic for tutorials
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,17 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if tutorial.videos.count > 0
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    elsif current_user.role == "admin"
+      redirect_to "/admin/tutorials/#{tutorial.id}/edit"
+    else
+      flash[:error] = 'This tutorial is still in progress.'
+      redirect_to '/tutorials'
+    end
+  end
+
+  def index
+    @tutorials = Tutorial.all
   end
 end
